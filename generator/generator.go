c56822eb471b9377eada00475db64a497a97d48e
3n0ugh <serhatkarabulut@protonmail.com>
2022-04-30 16:39:45 +0300

    :sparkles: Implement CSV reader into generator

--- generator/generator.go
+++ generator/generator.go
@@ -1,21 +1,43 @@
 package generator
 
 import (
+	"encoding/csv"
 	"fmt"
+	"github.com/3n0ugh/gen-populus/internal/config"
+	"github.com/pkg/errors"
+	"log"
 	"math/rand"
+	"os"
 	"sync"
 	"time"
 )
 
-func Generate(population uint64) error {
-	femaleNames := [][]string{{"Zeynep"}, {"Melek"}, {"Ayse"}}
-	maleNames := [][]string{{"Cem"}, {"Serhat"}, {"Ali"}}
-	lastNames := [][]string{{"Yilmaz"}, {"Karabulut"}, {"Arslan"}}
+func Generate(cfg config.Config) error {
+	femaleNameFile, closer, err := OpenFile(cfg.FemaleNameFile)
+	defer closer()
+	if err != nil {
+		return err
+	}
+	femaleNames, err := csv.NewReader(femaleNameFile).ReadAll()
 
-	femaleCount := uint64(float64(population) * 0.5)
-	maleCount := uint64(float64(population) * 0.5)
+	maleNameFile, closer, err := OpenFile(cfg.MaleNameFile)
+	defer closer()
+	if err != nil {
+		return err
+	}
+	maleNames, err := csv.NewReader(maleNameFile).ReadAll()
 
-	var nameChan = make(chan string, population)
+	lastnameFile, closer, err := OpenFile(cfg.LastnameFile)
+	defer closer()
+	if err != nil {
+		return err
+	}
+	lastnames, err := csv.NewReader(lastnameFile).ReadAll()
+
+	femaleCount := uint64(float64(cfg.TotalPopulation) * 0.5)
+	maleCount := uint64(float64(cfg.TotalPopulation) * 0.5)
+
+	var nameChan = make(chan string, cfg.TotalPopulation)
 
 	var wg sync.WaitGroup
 	wg.Add(2)
@@ -24,7 +46,7 @@ func Generate(population uint64) error {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		for i := uint64(0); i < femaleCount; i++ {
 			name := femaleNames[r.Intn(len(femaleNames))][0]
-			lastname := lastNames[r.Intn(len(lastNames))][0]
+			lastname := lastnames[r.Intn(len(lastnames))][0]
 			gender := "female"
 			age := r.Intn(110) + 1
 			ch <- fmt.Sprintf("%s\t%s\t%s\t%d", name, lastname, gender, age)
@@ -37,7 +59,7 @@ func Generate(population uint64) error {
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		for i := uint64(0); i < maleCount; i++ {
 			name := maleNames[r.Intn(len(femaleNames))][0]
-			lastname := lastNames[r.Intn(len(lastNames))][0]
+			lastname := lastnames[r.Intn(len(lastnames))][0]
 			gender := "male"
 			age := r.Intn(110) + 1
 			ch <- fmt.Sprintf("%s\t%s\t%s\t%d", name, lastname, gender, age)
@@ -53,4 +75,19 @@ func Generate(population uint64) error {
 	}
 
 	return nil
+}
+
+func OpenFile(filepath string) (*os.File, func(), error) {
+	file, err := os.Open(filepath)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to open csv file")
+	}
+
+	closeFunc := func() {
+		if err := file.Close(); err != nil {
+			log.Printf("failed to close csv file: %v", err)
+		}
+	}
+
+	return file, closeFunc, err
 }
