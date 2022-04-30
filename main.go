1c5386e94d6328f9c869f4b85cbd720317e310ed
3n0ugh <serhatkarabulut@protonmail.com>
2022-04-30 16:40:11 +0300

    :sparkles: Update generator function parameter

--- main.go
+++ main.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"github.com/3n0ugh/gen-populus/generator"
+	"github.com/3n0ugh/gen-populus/internal/config"
 	"log"
+	"os"
 )
 
 func main() {
-	err := generator.Generate(100)
+	// Open or if not exists create output file
+	file, err := os.OpenFile("data.csv", os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("failed to open file: %v", err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatalf("failed to close file: %v", err)
+		}
+	}()
+
+	// If file parameters are not specified, the default paths are used.
+	cfg, err := config.NewConfig(
+		1e7,
+		"",
+		"",
+		"",
+		file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = generator.Generate(cfg)
 	if err != nil {
 		log.Println(err)
 	}
