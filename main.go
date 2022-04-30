4bfcc5023d42fa2be81ba34dc56ae7e53ea0298a
3n0ugh <serhatkarabulut@protonmail.com>
2022-04-30 16:53:51 +0300

    :sparkles: Print the config and add timer

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/3n0ugh/gen-populus/internal/config"
 	"github.com/3n0ugh/gen-populus/pkg/generator"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	// Open or if not exists create output file
 	file, err := os.OpenFile("data.csv", os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -34,4 +38,17 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
+
+	since := time.Since(start)
+
+	// Prints the configuration information
+	conf, err := json.MarshalIndent(cfg, "", " ")
+	if err != nil {
+		log.Fatalf("failed to marshal config: %v", err)
+	}
+
+	fmt.Printf("%s\n", conf)
+	fmt.Printf("Generated %d data\n", cfg.TotalPopulation)
+	fmt.Println("Time elapsed:", since)
+
 }
